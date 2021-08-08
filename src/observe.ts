fd0220a4b56caef20a2df955e97f47533eff4033
jx3yang <65411020+jx3yang@users.noreply.github.com>
2021-08-08 04:01:28 -0400

    remove deprecated function

--- src/observe.ts
+++ src/observe.ts
@@ -1,36 +1,5 @@
 import { Content, RequestType, Request, ContentType } from './types'
 
-export const setUpObserversWithSender = (
-  sendContents: () => void,
-  parentNode: Element | null | undefined,
-  enabled: boolean,
-  urlPattern?: RegExp,
-) => {
-
-  let observersEnabled = enabled
-  const send = () => {
-    if (observersEnabled && (!urlPattern || urlPattern.test(window.location.href)))
-      sendContents()
-  }
-  // observe for dom changes
-  const observer = new MutationObserver(() => {
-    send()
-  })
-
-  if (parentNode)
-    observer.observe(parentNode, { childList: true, subtree: true, attributes: true })
-  // observe for tab focus change
-  document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'visible')
-      send()
-  })
-
-  return {
-    disable: () => { observersEnabled = false },
-    enable: () => { observersEnabled = true },
-  }
-}
-
 export const setUpObservers = (
   parseContents: () => Content[],
   parentNode: Element | null | undefined,
